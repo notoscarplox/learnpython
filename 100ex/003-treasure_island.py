62306fd4147f497254387a46b19463bb831bf06b
Oscar Alv <alv.oscarsilva@gmail.com>
2023-07-25 20:37:03 -0700

    lil fix

--- 100ex/003-treasure_island.py
+++ 100ex/003-treasure_island.py
@@ -2,18 +2,18 @@ print("Welcome Bitch\nFind the treasure fast")
 
 d1 = input("Wanna go right or left madafaka").lower()
 
-if d1 == "right":
+if d1 != "left":
     print("You are F dead\nGame Over")
 else:
     d2 = input("Wanna swimm or wait for boat").lower()
     if d2 != "wait":
         print("Atacked by a fat ass shark.\nGame Over")
     else:
-        d3 = input("Which door you wanna go retard").lower()
+        d3 = input("Which door you wanna go retard, red, yellow, or blue").lower()
         if d3 == "red":
             print("You dead burned.\n Game Over")
         elif d3 == "yellow":
-            print("Congrats you're a freak")
+            print("Congrats YOU WIN you're a freak")
         elif d3 == "blue":
             print("Traped in a room full of hungry beasts\n Game Over")
         else:
