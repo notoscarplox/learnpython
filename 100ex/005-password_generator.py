f91a1baf171f7d9a12ce43c97dde96f36475a37f
Oscar Alv <alv.oscarsilva@gmail.com>
2023-07-27 20:07:36 -0700

    added comments

--- 100ex/005-password_generator.py
+++ 100ex/005-password_generator.py
@@ -1,3 +1,7 @@
+#So I went through the hard way.
+#You can solve it also with random.choice() and randome.shuffle()
+
+
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -9,11 +13,12 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
-rand = 0
-letts_random = ""
-for let in range(0, int(nr_letters)):
-    rand = random.randint(0, len(letters) - 1)
-    letts_random += letters[rand]
+rand = 0                                                #Variable to store the random number that will be used as index for the list
+
+letts_random = ""                                       #Variable to store the letters selected
+for let in range(0, int(nr_letters)):                   #No. of times to pick a random letter wich is the number of letters choosed by input
+    rand = random.randint(0, len(letters) - 1)          #Random no. picked from 0 to the lenght of the list 'letters'
+    letts_random += letters[rand]                       #Picking a letter from the list with the random number picked before as index and adding it to the variable
 
 
 symb_random = ""
@@ -38,14 +43,4 @@ for n in range(0, lenpasswd):
     randomized_passwd += passwd[rand]
 
 
- 
-print(randomized_passwd)
-
-
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+ print(randomized_passwd)
