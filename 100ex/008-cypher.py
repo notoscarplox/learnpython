008ef3d7b702c73303958f1cf5fe2defef3e9267
Oscar Alv <alv.oscarsilva@gmail.com>
2023-07-29 21:21:49 -0700

    Combine both functions(encrypt and decrypt) into one. Trying pycharm

--- 100ex/008-cypher.py
+++ 100ex/008-cypher.py
@@ -1,19 +1,21 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-def encrypt(txt, shf):
+def encrypt(txt, shf, dir):
 	etext = ""
 	for n in txt:
 		x = 0
 		for i in alphabet:
 			if n == i:
-				etext += alphabet[x + shf]
+				if dir == "encode":
+					etext += alphabet[x + shf]
+				else:
+					etext += alphabet[x - shf]
 				break
 			x += 1
-	print(etext)		
+	print(etext)
 
-encrypt(txt = text, shf = shift)
+encrypt(txt=text, shf=shift, dir=direction)
