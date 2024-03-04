b262c7bb2c783bf03cfe2d3332f93c491f5e50a7
notoscarplox <alv.oscarsilva@gmail.com>
2024-03-04 16:18:26 -0700

    Added a child class

--- 100ex/carClass.py
+++ 100ex/carClass.py
@@ -33,20 +33,37 @@ class Car:
         self.odometer += miles
 
 
-my_car = Car("sudi", "b6", 2019)
-print(my_car.get_description())
-my_car.odometer = 23  # Modify attribute's value directly
-my_car.read_odometer()
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery_size = 40
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
 
-my_used_car = Car("bmw", "b6", 2020)
-print(my_used_car.get_description())
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_description())
+my_leaf.describe_battery()
 
-my_used_car.update_odometer(655)  # Modify attribute's value through a method
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(100)  # Method to increment odometer
-my_used_car.read_odometer()
-
-
-
+# my_car = Car("sudi", "b6", 2019)
+# print(my_car.get_description())
+# my_car.odometer = 23  # Modify attribute's value directly
+# my_car.read_odometer()
+#
+#
+# my_used_car = Car("bmw", "b6", 2020)
+# print(my_used_car.get_description())
+#
+# my_used_car.update_odometer(655)  # Modify attribute's value through a method
+# my_used_car.read_odometer()
+#
+# my_used_car.increment_odometer(100)  # Method to increment odometer
+# my_used_car.read_odometer()
