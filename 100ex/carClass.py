b3e80a846eca0e59c39230d63ea70e946ad67c3c
notoscarplox <alv.oscarsilva@gmail.com>
2024-03-04 15:34:30 -0700

    Update restaurant, added a user class to count the loging attempts

---
+++ 100ex/carClass.py
@@ -0,0 +1,52 @@
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer = 0  # Default value for an attribute
+
+    def get_description(self):
+        """Return neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    # Method to read odometer
+    def read_odometer(self):
+        """Print a statement showing the car's mileage"""
+        print(f"This car has {self.odometer} miles")
+
+    # Method used to update the odometer
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value"""
+        if mileage < self.odometer:
+            print("Can't rollback the odometer")
+        else:
+            self.odometer = mileage
+
+    # Method to increment odometer
+    def increment_odometer(self, miles):
+        """Add miles to odometer"""
+        self.odometer += miles
+
+
+my_car = Car("sudi", "b6", 2019)
+print(my_car.get_description())
+my_car.odometer = 23  # Modify attribute's value directly
+my_car.read_odometer()
+
+
+my_used_car = Car("bmw", "b6", 2020)
+print(my_used_car.get_description())
+
+my_used_car.update_odometer(655)  # Modify attribute's value through a method
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)  # Method to increment odometer
+my_used_car.read_odometer()
+
+
+
--- 100ex/restauantCalass.py
+++ 100ex/restauantCalass.py
@@ -19,11 +19,23 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        self.logins = 0
 
     def describe(self):
         print(f"This is {self.first_name.capitalize()} {self.last_name.capitalize()} and he is {self.age}")
 
+    def increment_loging_attempts(self):
+        self.logins += 1
+        return self.logins
+
+    def reset_loging_attempts(self):
+        self.logins = 0
+
 
 user1 = User("oscar", "alvarez", 30)
-
 user1.describe()
+
+print(user1.logins)
+user1.increment_loging_attempts()
+user1.increment_loging_attempts()
+print(user1.logins)
