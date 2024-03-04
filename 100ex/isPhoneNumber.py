531b8c3ceb614c4232ec0df669a7be2ebb75aae1
notoscarplox <alv.oscarsilva@gmail.com>
2024-03-04 09:21:25 -0700

    program to check if a phone number is in text using .isdecimal()

--- 100ex/isPhoneNumber.py
+++ 100ex/isPhoneNumber.py
@@ -4,7 +4,7 @@ def isPhoneNumber(text):
     for i in range(0, 3):
         if not text[i].isdecimal():
             return False
-    if text[4] != "-":
+    if text[3] != "-":
         return False
     for i in range(4, 7):
         if not text[i].isdecimal():
@@ -18,7 +18,9 @@ def isPhoneNumber(text):
     return True
 
 
-print("Is 415-555-4242 a phone number?")
-print(isPhoneNumber("415-555-4242"))
-print("Is Moshi moshi a phone number?")
-print(isPhoneNumber("Moshi moshi"))
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: ' + chunk)
+print('Done')
